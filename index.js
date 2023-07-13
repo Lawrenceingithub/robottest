215fb409c261fd3171d79e7d1bfc6a3aaca8efcc
Lawrence Fong <lawrencehei@hotmail.com>
2023-07-13 15:35:15 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-const webdriver = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
+const { Builder, By, until } = require('selenium-webdriver');
 
-(async () => {const { Builder, By, Key, until } = require('selenium-webdriver');
+(async function myFunction() {
+  // 创建一个driver实例
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    // 1. 跳转到百度
+    await driver.get('https://humanmade.jp/products/hm25te0713');
 
-// 创建一个 Chrome WebDriver 实例
-let driver = new Builder().forBrowser('chrome').build();
+    // 2. 搜索
+    //let searchText = 'selenium';
+    // 定位到搜索框, 并输入关键字
+    //await driver.findElement(By.id('kw')).sendKeys(searchText);
+    await new Promise(res => setTimeout(res, 1000));
 
-// 加载网页
-driver.get('https://www.fsm.gov.mo/');
+    //定位到購買按钮, 并点击
+    await driver.findElement(By.className('recommendation-modal__button')).click();
+    await driver.findElement(By.id('nomalbtn')).click();
+    await driver.findElement(By.name('checkout')).click();
 
-try {
-  // 等待 id 为 cht 的元素可见
-  let element = await driver.wait(until.elementLocated(By.id('cht')), 10000);
-  // 点击 id 为 cht 的元素
-  await element.click();
-} finally {
-  // 关闭浏览器
-  await driver.quit();
-}
+
+    // 3. 从结果列表中选择百度百科
+    //let containers = await driver.wait(until.elementsLocated(By.className('c-container')), 2000);
+    //let targetElement = null;
+    //for (let container of containers) {
+    //  let element = await container.findElement(By.css('h3>a'));
+    //  let title = await element.getText();
+    //  if (title.indexOf('百度百科') > -1) {
+    //    targetElement = element;
+    //    break;
+    //  }
+    //}
+    //if (targetElement) {
+      // 4. 打开百度百科
+    //  await targetElement.click();
+      // 切换window handle
+    //  let windows = await driver.getAllWindowHandles();
+    //  await driver.switchTo().window(windows[1]);
+
+    //  await driver.wait(until.elementLocated(By.className('main-content')), 5000);
+    //  await new Promise(res => setTimeout(res, 2000));
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // 关闭浏览器
+  }
 })();
