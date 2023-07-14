1743c081321e05ac181628f5f358eafaf69a645e
Lawrence Fong <lawrencehei@hotmail.com>
2023-07-14 17:44:08 +0800

    update

--- index.js
+++ index.js
@@ -4,44 +4,38 @@ const { Builder, By, until } = require('selenium-webdriver');
   // 创建一个driver实例
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    // 1. 跳转到百度
-    await driver.get('https://humanmade.jp/products/hm25te0713');
-
-    // 2. 搜索
-    //let searchText = 'selenium';
-    // 定位到搜索框, 并输入关键字
-    //await driver.findElement(By.id('kw')).sendKeys(searchText);
+    // 跳转到页面
+    await driver.get('https://humanmade.jp/products/hm25te0714');
     await new Promise(res => setTimeout(res, 1000));
 
     //定位到購買按钮, 并点击
     await driver.findElement(By.className('recommendation-modal__button')).click();
     await driver.findElement(By.id('nomalbtn')).click();
-    await driver.findElement(By.name('checkout')).click();
+    await new Promise(res => setTimeout(res, 2000));
+
+    await driver.wait(until.elementLocated(By.className('button button--primary button--full-width')));
+    const button = await driver.findElement(By.className('button button--primary button--full-width'));
+    await button.click();
+
+    await new Promise(res => setTimeout(res, 1000));
+    // 等待并填写邮箱和密码
+    await driver.wait(until.elementLocated(By.id('CustomerEmail')));
+    const emailField = await driver.findElement(By.id('CustomerEmail'));
+    await emailField.sendKeys('lawrencehei@Hotmail.com');
+
+    await driver.wait(until.elementLocated(By.id('CustomerPassword')));
+    const passwordField = await driver.findElement(By.id('CustomerPassword'));
+    await passwordField.sendKeys('QWEasd123');
+
+    // 点击登录按钮
+    await driver.findElement(By.css('#customer_login button')).click();
 
 
-    // 3. 从结果列表中选择百度百科
-    //let containers = await driver.wait(until.elementsLocated(By.className('c-container')), 2000);
-    //let targetElement = null;
-    //for (let container of containers) {
-    //  let element = await container.findElement(By.css('h3>a'));
-    //  let title = await element.getText();
-    //  if (title.indexOf('百度百科') > -1) {
-    //    targetElement = element;
-    //    break;
-    //  }
-    //}
-    //if (targetElement) {
-      // 4. 打开百度百科
-    //  await targetElement.click();
-      // 切换window handle
-    //  let windows = await driver.getAllWindowHandles();
-    //  await driver.switchTo().window(windows[1]);
-
-    //  await driver.wait(until.elementLocated(By.className('main-content')), 5000);
-    //  await new Promise(res => setTimeout(res, 2000));
+    console.log('登录成功！');
   } catch (error) {
-    console.error(error);
-  } finally {
+    console.error('登录失败：', error);
+  }  finally {
     // 关闭浏览器
+    //await driver.quit();
   }
 })();
